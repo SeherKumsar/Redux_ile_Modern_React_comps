fa98cebf6ad033f157dbe2dff932f56bb58593ee
SeherKumsar99 <seher.kumsar@lotusaibilisim.com>
2024-03-07 02:22:56 +0300

    reminder on building lists

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import Accordion from "./components/Accordion";
 function App() {
   const items = [
     {
+      id: 'qwe5465',
       label: 'Can I use React on a project?',
       content: 'You can use React on any project you want.'
     },
     {
+      id: 'xcv4567',
       label: 'Can I use JavaScript on a project?',
       content: 'You can use React on any project you want.'
     },
     {
+      id: 'bnmbn74125',
       label: 'Can I use CSS on a project?',
       content: 'You can use React on any project you want.'
     },
