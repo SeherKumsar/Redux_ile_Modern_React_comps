5e8d71829918b862460179a0d65f6db0ac35b2c6
SeherKumsar99 <seher.kumsar@lotusaibilisim.com>
2024-03-07 10:27:19 +0300

    Variation on Event Handlers (nextIndex)

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,22 +1,41 @@
 import { useState } from "react";
 
 function Accordion({ items }) {
-    const [expandedIndex, setExpandedIndex] = useState(0);
+  const [expandedIndex, setExpandedIndex] = useState(0);
 
-    const renderedItems = items.map((item, index) => {
-        const isExpanded = index === expandedIndex;
+//   const handleClick = (index) => {
+//     setExpandedIndex(index);
+//   };
 
-        // const content = isExpanded && <div>{item.content}</div>
+  const handleClick = (nextIndex) => {
+    setExpandedIndex(nextIndex);
+  };
 
-        return (
-            <div key={item.id}>
-                <div onClick={() => setExpandedIndex(index)}>{item.label}</div>
-                {isExpanded && <div>{item.content}</div>}
-            </div>
-        );
-    });
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+    // const content = isExpanded && <div>{item.content}</div>
+    // const handleClick = () => {
+    //     setExpandedIndex(index);
+    // };
 
-    return <div>{renderedItems}</div>;
+    return (
+      <div key={item.id}>
+        <div
+          //   onClick={() => {
+          //     console.log(index);
+          //     setExpandedIndex(index);
+          //   }}
+        //   onClick={handleClick}
+            onClick={() => handleClick(index)}
+        >
+          {item.label}
+        </div>
+        {isExpanded && <div>{item.content}</div>}
+      </div>
+    );
+  });
+
+  return <div>{renderedItems}</div>;
 }
 
 export default Accordion;
