b6be73c9f18c53ef7d1f6709496d8d152b96fc62
SeherKumsar99 <seher.kumsar@lotusaibilisim.com>
2024-03-07 03:52:03 +0300

    Finding the Expanded Item

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,5 +1,18 @@
+import { useState } from "react";
+
 function Accordion({ items }) {
-    const renderedItems = items.map((item) => {
+    const [expandedIndex, setExpandedIndex] = useState(0);
+
+    const renderedItems = items.map((item, index) => {
+        // if(index === expandedIndex) {
+        //     console.log('expanded');
+        // } else {
+        //     console.log('collapsed');
+        // }
+        const isExpanded = index === expandedIndex;
+
+        // console.log(isExpanded);
+
         return (
             <div key={item.id}>
                 <div>{item.label}</div>
