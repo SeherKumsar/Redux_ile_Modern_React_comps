8920e97d808ca63174271cbb09ec5115c85bf106
SeherKumsar99 <seher.kumsar@lotusaibilisim.com>
2024-03-07 04:01:13 +0300

    Conditional Rendering

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -4,19 +4,14 @@ function Accordion({ items }) {
     const [expandedIndex, setExpandedIndex] = useState(0);
 
     const renderedItems = items.map((item, index) => {
-        // if(index === expandedIndex) {
-        //     console.log('expanded');
-        // } else {
-        //     console.log('collapsed');
-        // }
         const isExpanded = index === expandedIndex;
 
-        // console.log(isExpanded);
+        // const content = isExpanded && <div>{item.content}</div>
 
         return (
             <div key={item.id}>
                 <div>{item.label}</div>
-                <div>{item.content}</div>
+                {isExpanded && <div>{item.content}</div>}
             </div>
         );
     });
