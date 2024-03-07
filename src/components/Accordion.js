d13e51073418d6bc1c99b37820e61a3077b3cc5d
SeherKumsar99 <seher.kumsar@lotusaibilisim.com>
2024-03-07 11:02:19 +0300

    Functional State Updates for Accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -2,10 +2,24 @@ import { useState } from "react";
 import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 function Accordion({ items }) {
-  const [expandedIndex, setExpandedIndex] = useState(0);
+  const [expandedIndex, setExpandedIndex] = useState(-1);
 
   const handleClick = (nextIndex) => {
-    setExpandedIndex(nextIndex);
+    console.log('STALE version of expandedIndex: ', expandedIndex);
+    
+    setExpandedIndex((currentExpandedIndex) => {
+        console.log('UP TO DATE version of expandedIndex: ', currentExpandedIndex);
+        if(currentExpandedIndex === nextIndex) {
+            return -1;
+        } else {
+            return nextIndex;
+        }
+    });
+    // if (expandedIndex === nextIndex) {
+    //     setExpandedIndex(-1);
+    // } else {
+    //     setExpandedIndex(nextIndex);
+    // }
   };
 
   const renderedItems = items.map((item, index) => {
