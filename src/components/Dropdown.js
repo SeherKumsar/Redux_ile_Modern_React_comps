e8d6fba2ffe61665631ea4bcf45e8c66ff5cdfac
SeherKumsar99 <seher.kumsar@lotusaibilisim.com>
2024-03-08 12:18:20 +0300

    Creating the Reusable Panel

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,38 +1,50 @@
-import { useState } from 'react';
-import { GoChevronDown } from 'react-icons/go';
+import { useState } from "react";
+import { GoChevronDown } from "react-icons/go";
+import Panel from "./Panel";
 
-function Dropdown({ options, value, onChange}) {
-    const [isOpen, setIsOpen] = useState(false);
+function Dropdown({ options, value, onChange }) {
+  const [isOpen, setIsOpen] = useState(false);
 
-    const handleClick = () => {
-        setIsOpen(!isOpen);
-        // setIsOpen((currentIsOpen) => !currentIsOpen);
-    };
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+    // setIsOpen((currentIsOpen) => !currentIsOpen);
+  };
 
-    const handleOptionClick = (option) => {
-        // CLOSE DROPDOWN
-        setIsOpen(false);
-        // WHAT OPTION DID THE USER CLICK ON??
-        onChange(option);
-    };
+  const handleOptionClick = (option) => {
+    // CLOSE DROPDOWN
+    setIsOpen(false);
+    // WHAT OPTION DID THE USER CLICK ON??
+    onChange(option);
+  };
 
-
-    const renderedOptions = options.map((option) => {
-      return <div className="hover:bg-sky-100 rounded cursor-pointer p-1" onClick={() => handleOptionClick(option)} key={option.value}>{option.label}</div>;
-    });
-
+  const renderedOptions = options.map((option) => {
     return (
-        <div className='w-48 relative'>
-            {/* <div onClick={handleClick}>{content}</div> */}
-            <div className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full" onClick={handleClick}>
-                {value?.label || "Select..."}
-                <GoChevronDown className='text-lg' />
-            </div>
-            {isOpen && <div className='absolute top-full border rounded p-3 shadow bg-white w-full'>{renderedOptions}</div>}
-        </div>
-        // ilk parametre false ise ifadenin tamamı false
-        // ikinci parametre true ise ifadenin tamamı true
+      <div
+        className="hover:bg-sky-100 rounded cursor-pointer p-1"
+        onClick={() => handleOptionClick(option)}
+        key={option.value}
+      >
+        {option.label}
+      </div>
     );
+  });
+
+  return (
+    <div className="w-48 relative">
+      <Panel
+        className="flex justify-between items-center cursor-pointer"
+        onClick={handleClick}
+      >
+        {value?.label || "Select..."}
+        <GoChevronDown className="text-lg" />
+      </Panel>
+      {isOpen && (
+        <Panel className="absolute top-full">
+          {renderedOptions}
+        </Panel>
+      )}
+    </div>
+  );
 }
 
 export default Dropdown;
