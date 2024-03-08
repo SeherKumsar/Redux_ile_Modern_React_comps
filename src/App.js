9d7899e8eeae3ae8da9f4ec8c3ba0b8cd2a649ac
SeherKumsar99 <seher.kumsar@lotusaibilisim.com>
2024-03-08 14:25:39 +0300

    Traditional Browser Navigation

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ function App() {
   // Seçili olan öge value olarak seçilir ve kullanıcı seçimi değiştirdiğinde aşağıya prob lar çağrılır.
   return (
     <div className="flex">
+      <a href="/dashboard">Dashboard</a>
       <Dropdown options={options} value={selection} onChange={handleSelect} />
       <Dropdown options={options} value={selection} onChange={handleSelect} />
     </div>
