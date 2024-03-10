c5223e4114a4567d2b6ae31676578ae0f83c9f1b
SeherKumsar <seherkumsar99@outlook.com>
2024-03-11 01:57:08 +0300

    Navigation Context (Provider)

--- src/App.js
+++ src/App.js
@@ -1,35 +1,5 @@
-import { useState } from "react";
-import Dropdown from "./components/Dropdown";
-
 function App() {
-  const [selection, setSelection] = useState(null);
-
-  const handleSelect = (option) => {
-    setSelection(option);
-  };
-
-  const options = [
-    {
-      label: 'Red',
-      value: 'red',
-    },
-    {
-      label: 'Green',
-      value: 'green',
-    },
-    {
-      label: 'Blue',
-      value: 'blue',
-    },
-  ];
-  // Seçili olan öge value olarak seçilir ve kullanıcı seçimi değiştirdiğinde aşağıya prob lar çağrılır.
-  return (
-    <div className="flex">
-      <a href="/dashboard">Dashboard</a>
-      <Dropdown options={options} value={selection} onChange={handleSelect} />
-      <Dropdown options={options} value={selection} onChange={handleSelect} />
-    </div>
-  );
+  return <div>App</div>;
 }
 
 export default App;
