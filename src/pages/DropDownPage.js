4bc070d3b2ad5c0c01b6484ac58fc19ddc11924a
SeherKumsar <seherkumsar99@outlook.com>
2024-03-11 01:19:06 +0300

    Extracting the DropdownPage

---
+++ src/pages/DropDownPage.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import Dropdown from "../components/Dropdown";
+
+function DropdownPage() {
+  const [selection, setSelection] = useState(null);
+
+  const handleSelect = (option) => {
+    setSelection(option);
+  };
+
+  const options = [
+    {
+      label: 'Red',
+      value: 'red',
+    },
+    {
+      label: 'Green',
+      value: 'green',
+    },
+    {
+      label: 'Blue',
+      value: 'blue',
+    },
+  ];
+  // Seçili olan öge value olarak seçilir ve kullanıcı seçimi değiştirdiğinde aşağıya prob lar çağrılır.
+  return (
+    <div className="flex">
+      <Dropdown options={options} value={selection} onChange={handleSelect} />
+    </div>
+  );
+}
+
+export default DropdownPage;
