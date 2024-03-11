53ade9163fda1636d6ca59e8aa0d9f2168e7dd16
SeherKumsar <seherkumsar99@outlook.com>
2024-03-11 11:20:10 +0300

    add route to App.js to show TablePage when a user goes to '/table'

--- src/App.js
+++ src/App.js
@@ -5,12 +5,11 @@ import AccordionPage from './pages/AccordionPage';
 import DropdownPage from './pages/DropDownPage';
 import ButtonPage from './pages/ButtonPage';
 import ModalPage from './pages/ModalPage';
+import TablePage from './pages/TablePage';
 
 function App() {
   return (
     <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
-      {/* <Link to="/accordion">Go to accordion</Link>
-      <Link to="/dropdown">Go to dropdown</Link> */}
       <Sidebar />
       <div className="col-span-5">
       <Route path="/accordion">
@@ -24,6 +23,9 @@ function App() {
         </Route>
         <Route path="/modal">
           <ModalPage />
+        </Route>
+        <Route path="/table">
+          <TablePage />
         </Route>
       </div>
     </div>
