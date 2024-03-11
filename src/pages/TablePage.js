7da9b613bf060077dc6b74e05cea5620570badda
SeherKumsar <seherkumsar99@outlook.com>
2024-03-11 13:31:11 +0300

    Fixed Cell Values

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,17 +1,26 @@
-import Table from '../components/Table';
+import Table from "../components/Table";
 
 function TablePage() {
   const data = [
-    { name: 'Orange', color: 'bg-orange-500', score: 5 },
-    { name: 'Apple', color: 'bg-red-500', score: 3 },
-    { name: 'Banana', color: 'bg-yellow-500', score: 1 },
-    { name: 'Lime', color: 'bg-green-500', score: 4 },
+    { name: "Orange", color: "bg-orange-500", score: 5 },
+    { name: "Apple", color: "bg-red-500", score: 3 },
+    { name: "Banana", color: "bg-yellow-500", score: 1 },
+    { name: "Lime", color: "bg-green-500", score: 4 },
   ];
 
   const config = [
-    { label: 'Name of Fruit' },
-    { label: 'Color' },
-    { label: 'Score' },
+    {
+      label: "Name",
+      render: (fruit) => fruit.name,
+    },
+    {
+      label: "Color",
+      render: (fruit) => fruit.color,
+    },
+    {
+      label: "Score",
+      render: (fruit) => fruit.score,
+    },
   ];
 
   return (
