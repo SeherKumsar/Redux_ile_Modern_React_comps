7faa7f27428607bb3f3703e0e417cdeb299c9b10
SeherKumsar <seherkumsar99@outlook.com>
2024-03-11 11:42:27 +0300

    Reminder on Table HTML Structure

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,17 @@
 function Table({ data }) {
-    return <div>{data.length}</div>;
+    return <table>
+        {/* <div>sadsad</div> */}
+        <thead>
+            <tr>
+                <th>Fruit</th>
+                <th>Color</th>
+                <th>Score</th>
+            </tr>
+        </thead>
+        <tbody>
+            
+        </tbody>                
+    </table>
 }
 
 export default Table;
