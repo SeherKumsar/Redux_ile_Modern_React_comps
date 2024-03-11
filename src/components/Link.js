9506c2dc77e6a457653fe2d0a1b3bf5b7295eeda
SeherKumsar <seherkumsar99@outlook.com>
2024-03-11 03:42:08 +0300

    Handling Control and Command Keys

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,16 +2,22 @@ import { useContext } from "react";
 import NavigationContext from "../context/navigation";
 
 function Link({ to, children }) {
-  // ilk parametre gideceği yol, ikinci parametre gösterilecek metin
   const { navigate } = useContext(NavigationContext);
-
+  // ilk parametre gideceği yol, ikinci parametre gösterilecek metin
   const handleClick = (event) => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
     event.preventDefault();
 
     navigate(to);
   };
 
-  return <a onClick={handleClick}>{children}</a>;
+  return (
+    <a href={to} onClick={handleClick}>
+      {children}
+    </a>
+  );
 }
 
 export default Link;
