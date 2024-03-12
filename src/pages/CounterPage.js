c7d2da7fdbcd9a01cd9885ae5b3bcfbfbcd6b46a
SeherKumsar <seherkumsar99@outlook.com>
2024-03-12 11:51:54 +0300

    find all the non-JSX expressions that refer to 1-2 related pieces of state

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -6,16 +6,21 @@ function useSomething() {
 }
 
 function CounterPage({ intialCount }) {
+    // initialize the state object
     const [count, setCount] = useState(intialCount);
 
+    // useEffect is a hook that allows you to perform side effects in function components
     useEffect(() =>{
         console.log(count);
     }, [count]);
 
+    // handleClick is a function that will be called when the button is clicked
     const handleClick = () => {
         setCount(count + 1);
     };
 
+    // handlePress is a function that will be called when the button is released
+    // JSX expressions must have one parent element
     return <div>
         <h1>Count is {count}</h1>
         <Button onClick={handleClick}>
