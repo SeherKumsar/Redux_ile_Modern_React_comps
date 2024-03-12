25e0d6f65c6aa24fc7adab6abbd8d2cac4afb855
SeherKumsar <seherkumsar99@outlook.com>
2024-03-12 10:02:47 +0300

    When user clicks the TH, sort data and pass the result to table

--- src/components/SortableTabje.js
+++ src/components/SortableTabje.js
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import Table from "./Table";
 
 function SortableTable(props) {
+
+  const [sortOrder, setSortOrder] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
+
   const { config } = props;
 
   const handleClick = (label) => {
-    console.log(`Sorting by ${label}`);
+    // console.log(`Sorting by ${label}`);
+    if (sortOrder === null) {
+      setSortOrder("asc");
+      setSortBy(label);
+    } else if (sortOrder === "asc") {
+      setSortOrder("desc");
+      setSortBy(label);
+    } else if (sortOrder === "desc") {
+      setSortOrder(null);
+      setSortBy(null);
+    }
   };
 
   const updatedConfig = config.map((column) => {
@@ -15,12 +30,19 @@ function SortableTable(props) {
     return {
       ...column,
       // Which column should be selected for sorting
-      header: () => <th onClick = {() => handleClick(column.label)}>{column.label} IS SORTABLE</th>,
+      header: () => (
+        <th onClick={() => handleClick(column.label)}>
+          {column.label} IS SORTABLE
+        </th>
+      ),
     };
   });
 
-  return <Table {...props} config={updatedConfig} />;
-  // props should contain the following properties
+  return <div>
+    {sortOrder} - {sortBy}
+    <Table {...props} config={updatedConfig} />;
+    {/* props should contain the following properties */}
+  </div>
 }
 
 export default SortableTable;
