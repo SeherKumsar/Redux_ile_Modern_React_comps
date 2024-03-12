0f3d6152dd94ab6f2d266c1057c457b5fe1c8e75
SeherKumsar <seherkumsar99@outlook.com>
2024-03-12 09:47:04 +0300

    TH returned by the 'header' function should watch for click events

--- src/components/SortableTabje.js
+++ src/components/SortableTabje.js
@@ -3,15 +3,19 @@ import Table from "./Table";
 function SortableTable(props) {
   const { config } = props;
 
+  const handleClick = (label) => {
+    console.log(`Sorting by ${label}`);
+  };
+
   const updatedConfig = config.map((column) => {
     if (!column.sortValue) {
-      return column;
+      return column; // If the column is not sortable, return it as is
     }
 
     return {
       ...column,
       // Which column should be selected for sorting
-      header: () => <th>{column.label} IS SORTABLE</th>,
+      header: () => <th onClick = {() => handleClick(column.label)}>{column.label} IS SORTABLE</th>,
     };
   });
 
