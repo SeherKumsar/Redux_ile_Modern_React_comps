cd8afe4d70e2f6d7ab003eb5dcae41aea3d23b40
SeherKumsar <seherkumsar99@outlook.com>
2024-03-12 09:42:03 +0300

    SortableTable should find column objects with 'sortValue' and add a 'header' function to them

--- src/components/SortableTabje.js
+++ src/components/SortableTabje.js
@@ -1,9 +1,22 @@
-import Table from './Table';
+import Table from "./Table";
 
 function SortableTable(props) {
-  
+  const { config } = props;
 
-  return <Table {...props} />;
+  const updatedConfig = config.map((column) => {
+    if (!column.sortValue) {
+      return column;
+    }
+
+    return {
+      ...column,
+      // Which column should be selected for sorting
+      header: () => <th>{column.label} IS SORTABLE</th>,
+    };
+  });
+
+  return <Table {...props} config={updatedConfig} />;
+  // props should contain the following properties
 }
 
 export default SortableTable;
