c2c620637739c356aa59cf82754b49fcd2f157b7
SeherKumsar <seherkumsar99@outlook.com>
2024-03-12 13:22:06 +0300

    created brute-forca hook for Sorting

--- src/components/SortableTabje.js
+++ src/components/SortableTabje.js
@@ -1,32 +1,12 @@
-import { useState } from "react";
+
 import { GoArrowSmallDown, GoArrowSmallUp } from 'react-icons/go';
 import Table from "./Table";
 
 function SortableTable(props) {
-  const [sortOrder, setSortOrder] = useState(null);
-  const [sortBy, setSortBy] = useState(null);
-
+  
   const { config, data } = props;
 
-  const handleClick = (label) => {
-    if (sortBy && label !== sortBy) {
-        setSortOrder("asc");
-        setSortBy(label);
-        return;
-    }
-    
-
-    if (sortOrder === null) {
-      setSortOrder("asc");
-      setSortBy(label);
-    } else if (sortOrder === "asc") {
-      setSortOrder("desc");
-      setSortBy(label);
-    } else if (sortOrder === "desc") {
-      setSortOrder(null);
-      setSortBy(null);
-    }
-  };
+  
 
   const updatedConfig = config.map((column) => {
     if (!column.sortValue) {
@@ -49,27 +29,6 @@ function SortableTable(props) {
       ),
     };
   });
-
-  // Only sort data if sortOrder && sortBy are not null
-  // Make a copy of the 'data' prop
-  // Find the correct sortValue function and use it
-
-  let sortedData = data;
-  if (sortOrder && sortBy) {
-    const { sortValue } = config.find((column) => column.label === sortBy);
-    sortedData = [...data].sort((a, b) => {
-      const valueA = sortValue(a);
-      const valueB = sortValue(b);
-
-      const reverseOrder = sortOrder === 'asc' ? 1 : -1;
-
-      if (typeof valueA === 'string') {
-        return valueA.localeCompare(valueB) * reverseOrder;
-      } else {
-        return (valueA - valueB) * reverseOrder;
-      }
-    });
-  }
   
   return (
     <div>
