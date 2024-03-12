27793d39550cca6953cce7af1cffdd34e01395c5
SeherKumsar <seherkumsar99@outlook.com>
2024-03-12 11:54:03 +0300

    cut them all out, paste them into 'useSometihng'

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -2,23 +2,18 @@ import { useState, useEffect } from 'react';
 import Button from '../components/Button';
 
 function useSomething() {
-    
-}
-
-function CounterPage({ intialCount }) {
-    // initialize the state object
     const [count, setCount] = useState(intialCount);
 
-    // useEffect is a hook that allows you to perform side effects in function components
     useEffect(() =>{
         console.log(count);
     }, [count]);
 
-    // handleClick is a function that will be called when the button is clicked
     const handleClick = () => {
         setCount(count + 1);
     };
+}
 
+function CounterPage({ intialCount }) {
     // handlePress is a function that will be called when the button is released
     // JSX expressions must have one parent element
     return <div>
