d665a5f1bac49fe943de5d80cb88594b94af681a
SeherKumsar <seherkumsar99@outlook.com>
2024-03-12 11:04:51 +0300

    Determining Icon Set

--- src/components/SortableTabje.js
+++ src/components/SortableTabje.js
@@ -31,7 +31,8 @@ function SortableTable(props) {
       // Which column should be selected for sorting
       header: () => (
         <th onClick={() => handleClick(column.label)}>
-          {column.label} IS SORTABLE
+          {getIcons(column.label, sortBy, sortOrder)}
+          {column.label}
         </th>
       ),
     };
@@ -65,6 +66,20 @@ function SortableTable(props) {
       {/* props should contain the following properties */}
     </div>
   );
+}
+
+function getIcons(label, sortBy, sortOrder) {
+  if (label !== sortBy) {
+    return "Show both icons";
+  }
+
+  if (sortOrder === null) {
+    return "Show both icons";
+  }  else if (sortOrder === "asc") {
+    return "Show up icon ⬆";
+  } else if (sortOrder === "desc") {
+    return "Show down icon ⬇";
+  }
 }
 
 export default SortableTable;
