6e7c334469ade50bb9efaba273785b8158cacfc5
SeherKumsar <seherkumsar99@outlook.com>
2024-03-13 09:56:08 +0300

    Refactoring to a Switch

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -7,22 +7,36 @@ const INCREMENT_COUNT = 'increment';
 const SET_VALUE_TO_ADD = 'change_value_to_add';
 
 const reducer = (state, action) => {
-
-  if (action.type === INCREMENT_COUNT) {
-    return {
-      ...state,
-      count: state.count + 1,
-    };
+  switch (action.type) {
+    case INCREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case SET_VALUE_TO_ADD:
+      return {
+        ...state,
+        valueToAdd: action.payload,
+      };
+    default:
+      // throw new Error('unexpected action type' + action.type);
+      return state;
   }
+  // if (action.type === INCREMENT_COUNT) {
+  //   return {
+  //     ...state,
+  //     count: state.count + 1,
+  //   };
+  // }
 
-  if (action.type === SET_VALUE_TO_ADD) {
-    return {
-      ...state,
-      valueToAdd: action.payload,
-    };
-  }
+  // if (action.type === SET_VALUE_TO_ADD) {
+  //   return {
+  //     ...state,
+  //     valueToAdd: action.payload,
+  //   };
+  // }
 
-  return state;
+  // return state;
 };
 
 function CounterPage({ initialCount }) {
